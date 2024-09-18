3fca78cca0f4643c07308a58b3a0cec73eb92b30
Burooj <burooj.bj@gmail.com>
2024-09-17 20:21:47 -0400

    add init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="styles/site.css" />
+
+	<!--  -->
+	<meta
+		name="description"
+		content="This contains design tokens that are used to create the visual design of the site. #todo" />
+	<meta property="og:image" content="$todo" />
+	<title>Pizza Nano</title>
+</head>
+
+<body>
+	<header></header>
+
+	<main>
+
+
+		<?php include 'modules/hero-landing/hero-landing.php'; ?>
+
+
+
+	</main>
+
+	<footer></footer>
+</body>
+
+</html>
