c47eabf45666e8f39e76214bad4fa82feb8289f2
Burooj <burooj.bj@gmail.com>
2024-09-17 20:23:29 -0400

    Add files via upload

---
+++ hero-landing/hero-landing.php
@@ -0,0 +1,25 @@
+<section class="hero">
+	<inner-column>
+
+
+		<div class="text-content">
+			<h1 class="attention-voice">
+				Taste the Nano Difference
+			</h1>
+
+			<h2 class="booming-voice">
+				Pizza, Fresh and Personal!
+			</h2>
+
+			<p class="notice-voice">
+				At Pizza Nano, we craft every pizza with love, using fresh ingredients and family recipes to bring you flavor in every bite. Made for you, made to share!
+			</p>
+		</div>
+
+
+		<picture class="hero-graphic">
+			<img src="images/hero.png" alt="">
+		</picture>
+
+	</inner-column>
+</section>
