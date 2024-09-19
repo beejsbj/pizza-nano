91da854bfc64714245370051acf60603dca9e9a8
Mohammed Shaikh <shaikm66@gmail.com>
2024-09-18 20:10:22 -0400

    c

---
+++ modules/specials/specials.php
@@ -0,0 +1,63 @@
+<section class="specials">
+	<inner-column>
+		<div class="header-text">
+			<h2 class="booming-voice">Pick-up Specials</h2>
+			<h3 class="loud-voice">Delicious Deals You Can't Resist.</h3>
+		</div>
+
+		<div class="pick-up">
+			<div class="special-1 box">
+
+				<h2 class="booming-voice">
+					Medium
+				</h2>
+
+				<div class="specs">
+					<p>$7.99</p>
+					<p><mark>1</mark> topping</p>
+				</div>
+				
+			</div>
+
+			<div class="special-2 box">
+
+				<h2 class="booming-voice">
+					Medium
+				</h2>
+
+				<div class="specs">
+					<p>$7.99</p>
+					<p><mark>1</mark> topping</p>
+				</div>
+				
+			</div>
+
+			<div class="special-3 box">
+
+				<h2 class="booming-voice">
+					Medium
+				</h2>
+
+				<div class="specs">
+					<p>$7.99</p>
+					<p><mark>1</mark> topping</p>
+				</div>
+				
+			</div>
+
+			<div class="special-4 box">
+
+				<h2 class="booming-voice">
+					Medium
+				</h2>
+
+				<div class="specs">
+					<p>$7.99</p>
+					<p><mark>1</mark> topping</p>
+				</div>
+				
+			</div>
+		</div>
+
+	</inner-column>
+</section>
