8ae6ad0170a4aa979539d4091574a066c4d09d5e
Mohammed Shaikh <shaikm66@gmail.com>
2024-09-20 17:28:50 -0400

    added delivery section

---
+++ modules/delivery-section/delivery.php
@@ -0,0 +1,25 @@
+<section class="delivery">
+		<ul>
+			<li class='nano'>
+				<picture>
+					<img src="images/delivery-icons/call.svg" alt="">
+				</picture>
+			</li>
+			<li class='uber'>
+				<picture >
+					<img src="images/delivery-icons/uber-eats.svg" alt="">
+				</picture>
+			</li>
+			<li class='doordash'>
+				<picture>
+					<img src="images/delivery-icons/doordash.svg" alt="">
+				</picture>
+			</li>
+			<li class='skip'>
+				<picture>
+					<img src="images/delivery-icons/skip.svg" alt="">
+				</picture>
+			</li>
+		</ul>
+
+</section>
--- modules/reviews/reviews.php
+++ modules/reviews/reviews.php
@@ -5,32 +5,39 @@
 		[
 			'name' => 'Ravi K.',
 			'review' => 'Best pizza in town! The flavors are incredible, and you can really taste the freshness in every bite. The service was top-notch, and the atmosphere was perfect for a relaxed meal with friends. Cannot wait to try more from the menu!"',
-			'rating' => '4'
+			'rating' => '4',
+			'color' => 'red'
 		],
 		[
 			'name' => 'John T.',
 			'review' => ' I tried the margherita and the pepperoni, and both were absolutely delicious. Friendly staff and great service made the experience even better. I will definitely be coming back.',
-			'rating' => '5'
+			'rating' => '5',
+			'color' => 'orange'
+
 		],
 		[
 			'name' => 'Sarah P.',
 			'review' => ' I am obsessed with their BBQ chicken pizza! The toppings are fresh, and the flavor is perfectly balanced. The cheese pulls were epic, and the crust was cooked to perfection. ',
-			'rating' => '5'
+			'rating' => '5',
+			'color' => 'red'
 		],
 		[
 			'name' => 'Omar K.',
 			'review' => ' Incredible pizza! We ordered the veggie supreme, and it was loaded with fresh veggies and gooey cheese. The crust was thin but held up well to the toppings, and the flavors were spot on.',
-			'rating' => '5'
+			'rating' => '5',
+			'color' => 'orange'
 		],
 		[
 			'name' => 'Emily R.',
 			'review' => ' Absolutely love this pizza place! The dough is so fresh, and the toppings are generous. I had the classic cheese pizza, and it was simply divine. They also have a great selection of drinks and sides.',
-			'rating' => '4'
+			'rating' => '4',
+			'color' => 'red'
 		],
 		[
 			'name' => 'David K.',
 			'review' => ' If you are looking for pizza perfection, this is the place. The pizzas have an amazing smoky flavor, and the crust is light yet crispy. I ordered the meat lovers, and it was loaded with quality meats and cheese. ',
-			'rating' => '4'
+			'rating' => '4',
+			'color' => 'orange'
 		],
 	]
 
@@ -42,7 +49,7 @@
  		
 <?php foreach($reviews as $review) { ?>
 
- 		<div class="review-card">
+ 		<div class="review-card <?= $review['color'] ?>">
  			<p class="calm-voice">
  				<?= $review['review'] ?>
  			</p>
